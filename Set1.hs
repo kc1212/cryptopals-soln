308561d37302b2b95add867e1aead3817f2ef75e
Kelong Cong <kelongcong@gmail.com>
2014-11-21 00:34:10 +0000

    added file for set 4, improved set 3

--- Set1.hs
+++ Set1.hs
@@ -33,11 +33,14 @@ hexStr2a = "1c0111001f010100061a024b53535009181c"
 hexStr2b = "686974207468652062756c6c277320657965"
 hexStr2ans = "746865206b696420646f6e277420706c6179"
 
-hexXor :: String -> String -> B.ByteString -- TODO check length, should be equal
-hexXor a b = B.pack $ B.zipWith xor (decodeHexStr a) (decodeHexStr b)
+hexByteXor :: String -> B.ByteString -> B.ByteString
+hexByteXor a b = B.pack $ B.zipWith xor (decodeHexStr a) b
+
+hexHexXor :: String -> String -> B.ByteString
+hexHexXor a b = hexByteXor a (decodeHexStr b)
 
 testChallenge2 :: Bool
-testChallenge2 = hexXor hexStr2a hexStr2b == decodeHexStr hexStr2ans
+testChallenge2 = hexHexXor hexStr2a hexStr2b == decodeHexStr hexStr2ans
 
 
 -- challenge 3
@@ -54,25 +57,24 @@ isEnglish str =
         alphaRatio = getRatio 'a'
         getRatio x = toRational (length (filter (==x) str)) / len
     in
+        foldr (\x xs -> isPrint x && isPrint x && xs) True str &&
         spaceRatio > 0.09 && spaceRatio < 0.2 &&
         echoRatio  > 0.06 && echoRatio  < 0.2 ||
         alphaRatio > 0.05 && alphaRatio < 0.2
 
-repeatN 0 _ = []
-repeatN x xs = repeatN (x-1) xs ++ xs
-
-findXorKey :: String -> [(Bool,String,String)]
+findXorKey :: String -> [(Bool,Char,String)]
 findXorKey str =
     let
-        hexChars = [ repeatN (len `div` 2) $ x:[y] | x <- asciiChars, y <- asciiChars ]
-        asciiChars = ['0'..'9'] ++ ['a'..'f']
+        keys = [chr x | x <- [20..126]]
         len = length str
     in filter (\(x,_,_) -> x) $
-            map (\key -> let str' = BC8.unpack $ hexXor str key
-                    in (isEnglish str', key, str')) hexChars
+            map (\key -> let str' = BC8.unpack $ hexByteXor str (BC8.pack $ replicate len key)
+                         in (isEnglish str', key, str'))
+                keys
 
 testChallenge3 = findXorKey hexStr3 -- one of the result is in English
 
+-- challenge 4
 
 
 
