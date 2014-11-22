9db0f4cc0f968356104eb78a372b07283d659228
Kelong Cong <kelongcong@gmail.com>
2014-11-22 22:32:07 +0000

    added answers to comments

--- Set1.hs
+++ Set1.hs
@@ -3,13 +3,15 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Base64.Lazy as B64
 import qualified Data.ByteString.Base16.Lazy as B16
 import qualified Data.ByteString.Lazy.Char8 as BC8
+import Data.Char (chr, isAscii, isPrint)
 import Data.Bits
-import Data.Char
 
 
 -- challenge 1
+
 hexStr1 :: String
 hexStr1 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+-- I'm killing your brain like a poisonous mushroom
 
 base64Str1 :: String
 base64Str1 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
@@ -41,10 +43,12 @@ hexHexXor a b = hexByteXor a (decodeHexStr b)
 
 testChallenge2 :: Bool
 testChallenge2 = hexHexXor hexStr2a hexStr2b == decodeHexStr hexStr2ans
+-- result of xor should be "the kid don't play"
 
 
 -- challenge 3
 
+hexStr3 :: String
 hexStr3 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 -- we assume letter frequency analysis is feasible
@@ -57,7 +61,7 @@ isEnglish str =
         alphaRatio = getRatio 'a'
         getRatio x = toRational (length (filter (==x) str)) / len
     in
-        foldr (\x xs -> isPrint x && isPrint x && xs) True str &&
+        foldr (\x xs -> isAscii x && isPrint x && xs) True str &&
         spaceRatio > 0.09 && spaceRatio < 0.2 &&
         echoRatio  > 0.06 && echoRatio  < 0.2 ||
         alphaRatio > 0.05 && alphaRatio < 0.2
@@ -72,7 +76,9 @@ findXorKey str =
                          in (isEnglish str', key, str'))
                 keys
 
-testChallenge3 = findXorKey hexStr3 -- one of the result is in English
+testChallenge3 = findXorKey hexStr3
+-- one of the result is in English, which is
+-- Cooking MC's like a pound of bacon
 
 -- challenge 4
 
