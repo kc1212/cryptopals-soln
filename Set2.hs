a17221b3be59e9f345e185d04312c818f8393936
Kelong Cong <kelongcong@gmail.com>
2015-02-14 18:14:08 +0000

    challenge 12, all blocks

--- Set2.hs
+++ Set2.hs
@@ -73,8 +73,8 @@ genBytes n = do
 genKey :: IO AES
 genKey = genBytes 16 >>= return . initAES . B.toStrict
 
-encOracle :: AES -> Bool -> B.ByteString -> IO B.ByteString
-encOracle key isCbc pt = do
+ecbOracle11 :: AES -> Bool -> B.ByteString -> IO B.ByteString
+ecbOracle11 key isCbc pt = do
     before  <- getStdRandom (randomR (5,10)) >>= genBytes
     after   <- getStdRandom (randomR (5,10)) >>= genBytes
     iv      <- genBytes 16
@@ -94,13 +94,13 @@ findBlockSize key unkStr =
     in elemIndex True $ map (\x -> let xs = toChunksN 16 x in xs !! 0 == xs !! 1) ress
 
 -- the key is cipher text, value is plain text
-createPtCtMap :: AES -> B.ByteString -> Map.Map B.ByteString B.ByteString
-createPtCtMap key xs =
+createCtPtMap :: AES -> B.ByteString -> Map.Map B.ByteString B.ByteString
+createCtPtMap key xs =
     Map.fromList $ map (\x -> (myEncryptECB key x, x)) (map (B.snoc xs) [0..255])
 
-iDontLikeMaybe :: Maybe a -> a
-iDontLikeMaybe (Just a) = a
-iDontLikeMaybe Nothing = error "Nothing!"
+iHateMaybe :: Maybe a -> a
+iHateMaybe (Just a) = a
+iHateMaybe Nothing = error "I loathe Nothing more!"
 
 doChallenge12 :: IO ()
 doChallenge12 = do
@@ -108,21 +108,32 @@ doChallenge12 = do
     key <- genKey
 
     putStr "block size: "
-    let bs = iDontLikeMaybe $ fmap (fromIntegral . (+1)) (findBlockSize key unkText)
+    let bs = iHateMaybe $ fmap (fromIntegral . (+1)) (findBlockSize key unkText)
     putStrLn $ show $ bs
 
-    let ecbOracle k pt = myEncryptECB k $ pkcs7 bs (B.append pt unkText)
+    let ecbOracle12 pt = myEncryptECB key $ pkcs7 bs (B.append pt unkText)
 
     putStr "is ECB: "
-    putStrLn $ show $ byteStringHasRepeat $ ecbOracle key (B.replicate 64 12)
+    putStrLn $ show $ byteStringHasRepeat $ ecbOracle12 (B.replicate 64 12)
 
     let breakEcbSimple ctr pre =
-            let preMap = createPtCtMap key pre -- looks up value at a key
-                firstBlock = B.take bs (ecbOracle key (B.take ctr pre))
-                solvedBlock = iDontLikeMaybe $ Map.lookup firstBlock preMap
-            in if ctr == 0 then pre else breakEcbSimple (ctr-1) (B.tail solvedBlock)
+            let
+                preMap = createCtPtMap key pre
+                initBlock = B.take (B.length pre + 1) (ecbOracle12 (B.take ctr pre))
+                solvedBlock = Map.lookup initBlock preMap
+            in
+                if ctr == 0 || solvedBlock == Nothing
+                then pre
+                else breakEcbSimple (ctr-1) (B.tail (iHateMaybe solvedBlock))
 
-    putStrLn $ show $ breakEcbSimple bs (C8.replicate (bs-1) '-')
+    -- length should always be a multiple of 16 due to pkcs7 padding
+    let ctLen = B.length $ ecbOracle12 (C8.pack "")
+    putStrLn $ if mod ctLen bs == 0
+               then "starting decryptiong (" ++ show ctLen ++ ")..."
+               else error "ctLen not multiple of 16"
+
+    -- assuming the text won't start with \0
+    putStr $ C8.unpack $ B.dropWhile (==0) $ breakEcbSimple (ctLen-1) (B.replicate (ctLen-1) 0)
 
 
 type CookieObj = [(String,String)]
@@ -162,32 +173,32 @@ doChallenge13 = do
 
 
 main = do
-    print "challenge 9:"
+    putStrLn "challenge 9:"
     let res9 = pkcs7 20 (C8.pack "YELLOW SUBMARINE")
-    print $ res9
-    print $ B.unpack res9
+    putStrLn $ show res9
+    putStrLn $ show $ B.unpack res9
 
-    print "challenge 10:"
-    print $ testCBC
+    putStrLn "challenge 10:"
+    putStrLn $ show testCBC
     ct10 <- fmap (base64ToByteString . concat . lines) (readFile "10.txt")
     let key10 = initAES $ B.toStrict $ C8.pack "YELLOW SUBMARINE"
     let iv10 = C8.pack $ "0000000000000000"
     let pt10 = C8.unpack $ myDecryptCBC key10 iv10 ct10
     putStr pt10
 
-    print "challenge 11:"
+    putStrLn "challenge 11:"
     isCbc <- getStdRandom random
-    ct11 <- genKey >>= \key -> encOracle key isCbc (C8.pack pt10) -- TODO need randomly generate plain text
-    print $ if isCbc /= (byteStringHasRepeat ct11)
+    ct11 <- genKey >>= \key -> ecbOracle11 key isCbc (C8.pack pt10) -- TODO need randomly generate plain text
+    putStrLn $ if isCbc /= (byteStringHasRepeat ct11)
                 then "prediciton correct!" else "prediction wrong..."
 
-    print "challenge 12:"
+    putStrLn "challenge 12:"
     doChallenge12
 
-    print "challenge 13:"
+    putStrLn "challenge 13:"
     doChallenge13
 
-    print "done"
+    putStrLn "done"
 
 
 
