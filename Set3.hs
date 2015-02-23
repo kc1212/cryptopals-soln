8262efb54b1abc3c2351384d60a324419860d8b2
Kelong Cong <kelongcong@gmail.com>
2015-02-22 19:13:28 -0500

    padding oracle attack code type checks

--- Set3.hs
+++ Set3.hs
@@ -15,6 +15,30 @@ decPaddingOracle key iv ct =
         Nothing -> False
         _       -> True
 
+-- right align xor a block (b) with some bytes (x)
+modBlock :: B.ByteString -> B.ByteString -> B.ByteString
+modBlock b x =
+    let lenx = B.length x
+        b' = B.append (B.replicate (aesBs - lenx) 0) x
+    in case lenx > 16 || lenx < 1 of
+        True  -> error $ "'x' has bad length: " ++ show (lenx)
+        _     -> byteByteXor b' b
+
+allPaddings :: [B.ByteString]
+allPaddings = map (\x -> B.replicate x (fromIntegral x)) [1..aesBs]
+
+crackOneBlock :: B.ByteString -> (B.ByteString -> Bool) -> [B.ByteString] -> B.ByteString
+crackOneBlock inp oracle ct =
+    let l = B.length inp
+        (ct1,ct2) = splitAt (length ct - 2) ct
+        tryx x = modBlock (B.replicate (l+1) (fromIntegral $ l+1)) (B.cons x inp)
+        blox x = ct1 ++ [x `byteByteXor` head ct2] ++ tail ct2
+        res = filter (\(_,x) -> x)
+                (map ( \x -> (x, (oracle $ B.concat $ blox $ tryx x)) ) [0..255])
+    in if B.length inp == aesBs then inp
+        else if length res == 1 then crackOneBlock (B.cons (fst $ head res) inp) oracle ct
+        else error "something went wrong!"
+
 doChallenge17 = do
     key <- genKey
     iv  <- genBytes 16
@@ -23,16 +47,7 @@ doChallenge17 = do
     let pt = pts !! 0 -- TODO only attack one for now
     let ct = toChunksN aesBs (encPaddingOracle key iv pt)
 
-    let tries = [0..255]
-    let paddings = map (\x -> B.replicate x (fromIntegral x)) [1..aesBs]
-
-    -- TODO try to decrypt last block first, then refactor the code to do all blocks
-    let ctBlocks = length ct
-    let modBlock b x =
-            let b' = B.append (B.replicate (aesBs - B.length x) 0) x
-            in byteByteXor b' b
-
-    putStrLn $ show paddings
+    putStrLn $ show $ crackOneBlock (C8.pack "") (decPaddingOracle key iv) ct
 
 main = do
     doChallenge17
