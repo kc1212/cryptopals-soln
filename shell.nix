f59b30ce33f7d77de8561f131b53e0cedcab0f2f
kc <kc04bc@gmx.com>
2015-07-04 19:48:12 +0100

    added nix-shell environment

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+let
+  inherit (nixpkgs) pkgs;
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps;
+    [mtl random byteable cipher-aes base64-bytestring base16-bytestring split]);
+
+in
+pkgs.stdenv.mkDerivation {
+  name = "matasano";
+  buildInputs = [ ghc ];
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+}
+
